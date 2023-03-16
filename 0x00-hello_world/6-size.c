c46a4cee9306f6f07ffc3e082821faccdffbd281
syphrose <swabulwenyi@gmail.com>
2023-03-16 08:31:40 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	printf(""/n" Size of a char : 1  byte(s)", sizeof(char));
-	printf(""/n" Size of an int : 4 byte(s)", sizeof(int));
-	printf(""/n" Size of a float : 4 byte(s)", sizeof(float));
-	printf(""/n" Size of a long int : 4 byte(s)", sizeof(long int));
-	printf(""/n" Size of a long long int : 8 byte(s)", sizeof(long long int));
+	printf("" / n") Size of a char : 1  byte(s)", sizeof(char));
+	printf("" / n") Size of an int : 4 byte(s)", sizeof(int));
+	printf("" / n") Size of a float : 4 byte(s)", sizeof(float));
+	printf("" / n") Size of a long int : 4 byte(s)", sizeof(long int));
+	printf("" / n") Size of a long long int : 8 byte(s)", sizeof(long long int));
 	return (0);
 
 }
