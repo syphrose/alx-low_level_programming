30028c87175dcd4bb05424bb3b70ba0c4783bf74
syphrose <swabulwenyi@gmail.com>
2023-03-16 07:56:55 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int main(void)
 	printf("Size of an int: 4 byte(s)", sizeof(int));
 	printf("Size of a float: 4 byte(s)", sizeof(float));
 	printf("Size of a long int: 4 byte(s)", sizeof(long int));
+	printf("Size of a long long int: 8 byte(s)", sizeof(long long int));
 	return (0);
 
 }
