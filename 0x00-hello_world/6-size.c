df13b639ffe8aa1b6875fd7c5bd144cdb2765718
syphrose <swabulwenyi@gmail.com>
2023-03-16 08:35:42 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	printf("" / n") Size of a char : 1  byte(s)", sizeof(char));
+	printf("Size of a char : 1  byte(s)", sizeof(char));
 	printf("" / n") Size of an int : 4 byte(s)", sizeof(int));
 	printf("" / n") Size of a float : 4 byte(s)", sizeof(float));
 	printf("" / n") Size of a long int : 4 byte(s)", sizeof(long int));
