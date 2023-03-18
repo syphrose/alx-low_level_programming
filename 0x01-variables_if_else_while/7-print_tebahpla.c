6d291c078ae5377ccdb60303eea09384967607d1
syphrose <swabulwenyi@gmail.com>
2023-03-18 13:25:29 -0700

    A reversed output of alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - function input
+ *
+ * Return: equals 0
+ *
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
