1c77b921010606f17f987ebf119ebe2fafdb171d
syphrose <swabulwenyi@gmail.com>
2023-03-22 00:25:44 -0700

    putchar file with a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - a program that prints -putchar with a new line
+ *
+ * Return: should be = 0 to be a success
+ *
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
