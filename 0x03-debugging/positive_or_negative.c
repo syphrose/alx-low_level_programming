3581d15d95a99bea3f78d5d77d8163a57b3fa3f0
syphrose <swabulwenyi@gmail.com>
2023-03-23 10:52:44 -0700

    Checking for +ve & -ve function

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * positive_or_negative - checking for positive or negative numbers
+ *
+ * @i: number to be checked
+ *
+ * Return: 0
+ *
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 98)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
+}
+
