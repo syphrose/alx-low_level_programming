6782b097fad50d358b05862485f6b32baa4faed2
syphrose <swabulwenyi@gmail.com>
2023-03-23 05:23:32 -0700

    Sum of the multiples of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - function
+ *
+ * Return: equals 0
+ *
+ */
+
+int main(void)
+{
+	int c;
+	int sum;
+
+	sum = 0;
+
+	for (c = 0; c < 10; c++)
+	{
+		if ((c % 3 == 0) || (c % 5 == 0))
+		{
+			sum = sum + c;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
