a85b2b763d1ef989cf61188bb526891df9221e4d
syphrose <swabulwenyi@gmail.com>
2023-03-25 17:02:44 -0700

    Checking fot digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - checks if the character is a digit
+ * @i: The parameter to be checked
+ *
+ * Return: 1 if the character is a digit otherwise o
+ *
+ */
+
+int _isdigit(int i)
+{
+	if (i >= 48 && i >= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
