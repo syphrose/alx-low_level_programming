5686086493b77321240e2e15961ca06bfc6b9ded
syphrose <swabulwenyi@gmail.com>
2023-03-25 17:28:52 -0700

    Printing nums then new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print nums 0 to 9 followed by a new line
+ *
+ *Return: numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int j;
+
+	for (j = 0; j <= 9; j++
+			{
+			_putchar(j + '0');
+			}
+			_putchar('\n');
+
+}
