9ae20bd61a58c8c3865a4bb3e6c34d33731f24d8
syphrose <swabulwenyi@gmail.com>
2023-03-27 06:47:03 -0700

    Updating a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - taking a ptr to an int as a par and updating
+ * it's value to 98
+ * @n: parameter to be replaced
+ *
+ * Return: always 0
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
