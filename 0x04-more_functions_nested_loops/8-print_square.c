e795c146e45e297faac38349830d6fdbfabfd017
syphrose <swabulwenyi@gmail.com>
2023-03-27 03:04:48 -0700

    Printing squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,17 +13,17 @@ void print_square(int size)
 
 	if (size <= 0)
 	{
-	_putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
-	for (x = 0; x < size; x++)
+		for (x = 0; x < size; x++)
 	{
-	for (y = 0; y < size; y++)
+		for (y = 0; y < size; y++)
 	{
-	_putchar(36);
+		_putchar(36);
 	}
-	_putchar('\n');
+		_putchar('\n');
 	}
 	}
 }
