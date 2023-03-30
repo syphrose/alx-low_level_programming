b3e1d50531e25192a9dadebc8c2f51242e982097
syphrose <swabulwenyi@gmail.com>
2023-03-30 02:59:15 -0700

    Concatenation

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - concatenates upto n characters from the source
+ *string to the end of the destination
+ *@dest: a string
+ *@src: a string not to be terminated
+ *@n: number of chars to be appended
+ *
+ * Return: dest
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int destlen;
+	int srclen;
+	int i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		destlen++;
+	for (i = 0; src[i] != '\0'; i++)
+		srclen++;
+	for (i = 0; i < n; i++)
+		dest[destlen + i] = src[i];
+	return (dest);
+}
+
