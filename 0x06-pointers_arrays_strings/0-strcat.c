715a25bef39a53e6e2a262156e6872164de9e55a
syphrose <swabulwenyi@gmail.com>
2023-03-30 02:18:56 -0700

    Concatenating two strings.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -20,6 +20,6 @@ char *_strcat(char *dest, char *src)
 	for (i = 0; src[i] != '\0'; i++)
 		srclen++;
 	for (i = 0; i <= srclen; i++)
-		dest[destlen +i] = src[i];
+		dest[destlen + i] = src[i];
 	return (dest);
 }
