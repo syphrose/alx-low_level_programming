b0bafabba7970a0dfcd76d7ad3db928abf1510db
syphrose <swabulwenyi@gmail.com>
2023-04-01 07:25:50 -0700

    Header file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stddef.h>
-
+void print_number(int n);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, const char *src, size_t n);
@@ -11,5 +11,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 #endif
