36f3e4f46ef9902921a7c3ff4c4fd59bf049d1b0
syphrose <swabulwenyi@gmail.com>
2023-04-01 08:10:22 -0700

    replace a letter with the 13th letter

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * rot13 - substitution cipher that replaces a letter,
+ * with the 13th letter after it in the alphabet
+ * @s: pointer to the string
+ *
+ * Return: s
+ *
+ */
+char *rot13(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (j = 0; (s[i] >= 'a' && s[i] <= 'z') ||
+			(s[i] >= 'A' && s[i] <= 'Z');j++, i++)
+	/* main block of code
+    ..*/
+		{
+			if (j == 2)
+				j = 0;
+
+			if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+				s[i] += 13;
+			else
+				s[i] -= 13;
+
+			if (j == 1)
+				break;
+		}
+	}
+
+	return (s);
+}
+
