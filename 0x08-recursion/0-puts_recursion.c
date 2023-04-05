c926463dc7b95375d473b00d98c406d74fcd1a0b
syphrose <swabulwenyi@gmail.com>
2023-04-05 05:14:08 -0700

    Printing a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: string to be printed
+ * Return: 0(success)
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
