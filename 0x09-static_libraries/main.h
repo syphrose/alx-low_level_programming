e1872faebd06c974d8f2e4f55811a62e9f6048e9
syphrose <swabulwenyi@gmail.com>
2023-04-08 00:21:07 -0700

    Libmy file 3

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,9 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stddef.h>
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
-#ifndef MAIN_H
-#define MAIN_h
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -24,5 +24,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#include <stddef.h>
 #endif
