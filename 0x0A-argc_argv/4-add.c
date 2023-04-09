197892eb225eafebabb785de2fad7df296ab0a89
syphrose <swabulwenyi@gmail.com>
2023-04-09 15:40:16 -0700

    Addition of positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,15 +15,18 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 		printf("0\n");
 
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
 			printf("%s\n", "Error");
 			return (1);
+			}
 		}
 	}
-	for (k = 0; k < argc; k++)
+	for (k = 1; k < argc; k++)
 	{
 		num = atoi(argv[k]);
 		result += num;
