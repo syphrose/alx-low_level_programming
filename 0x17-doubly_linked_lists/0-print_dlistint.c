bea889ec3325692cc423763efb103a4b5d25d6e8
syphrose <swabulwenyi@gmail.com>
2023-06-15 19:49:09 -0700

    prints all the elements of a list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * size_t print_dlistint - prints all the elements of a list
+ * print_dlistint - prints all the elements of a list
  * @h: counts fron the head to count
  *
  * Return: count
@@ -12,7 +12,7 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *current = h;
 	size_t node_count = 0;
-	
+
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
