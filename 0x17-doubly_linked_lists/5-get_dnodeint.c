3717a3cc8c33e8bc868e6e5397fe1c67dc445d2d
syphrose <swabulwenyi@gmail.com>
2023-06-17 04:48:34 -0700

    returns the nth node of a list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,69 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
 /**
- * dlistint_t *get_dnodeint_at_index - returns the nth node of a
- * dlistint_t linked list
- * main - Create a sample doubly linked list
+ * dlistint_t *get_dnodeint_at_index - returns the nth node of a linked list
  * @head: pointer
- * @index: integers not signed
- * Return: NULL if the node does not exist
+ * @index: integer
+ * Return: NULL
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
-	dlistint_t *current = head;
-
-	while (current != NULL)
+	
+	while (head != NULL)
 	{
 		if (count == index)
 		{
-			return (current);
+			return (head);
 		}
-		current = current->next;
+		head = head->next;
 		count++;
 	}
 	return (NULL);
 }
-int main(void)
-{
-	dlistint_t *head = NULL;
-	dlistint_t *node1 = (dlistint_t *)malloc(sizeof(dlistint_t));
-	dlistint_t *node2 = (dlistint_t *)malloc(sizeof(dlistint_t));
-	dlistint_t *node3 = (dlistint_t *)malloc(sizeof(dlistint_t));
-	dlistint_t *node4 = (dlistint_t *)malloc(sizeof(dlistint_t));
-
-	node1->n = 1;
-	node1->prev = NULL;
-	node1->next = node2;
-
-	node2->n = 2;
-	node2->prev = node1;
-	node2->next = node3;
-
-	node3->n = 3;
-	node3->prev = node2;
-	node3->next = node4;
-
-	node4->n = 4;
-	node4->prev = node3;
-	node4->next = NULL;
-
-	head = node1;
-
-	dlistint_t *result = get_dnodeint_at_index(head, 2);
-
-	if result != NULL
-	{
-		printf("The value of the third node is: %d\n", result->n);
-	} else
-	{
-	    printf("Node not found.\n");
-	}
-	free(node1);
-	free(node2);
-	free(node3);
-	free(node4);
-
-	return (0);
-}
-
