7620278f803e20e66d8501561e7fd96cd7b36ccb
syphrose <swabulwenyi@gmail.com>
2023-06-17 04:23:05 -0700

    returns the sum of all the data (n) of a list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include "lists.h"
+/**
+ * sum_dlistint -  returns the sum of all the data
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return sum;
+}
